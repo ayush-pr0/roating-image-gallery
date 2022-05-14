50d3f879f3cf6246f457b630a908800770e487d1
ayush-geeks <ayushkhandelwal0608@gmail.com>
2022-05-14 23:51:23 +0530

    Slide show added

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const imgEl = document.querySelector(".image-container");
 
 const prevEl = document.getElementById("prev");
 const nextEl = document.getElementById("next");
+const slideShowEl = document.getElementById("slide-show");
+let auto = false;
 let x = 0;
 
 const updateGallery = function () {
@@ -9,11 +11,29 @@ const updateGallery = function () {
 };
 
 prevEl.addEventListener("click", () => {
+    auto = false;
+    if (myInterval) clearInterval(myInterval);
     x += 45;
     updateGallery();
 });
 
 nextEl.addEventListener("click", () => {
+    auto = false;
+    if (myInterval) clearInterval(myInterval);
     x -= 45;
     updateGallery();
+});
+
+let myInterval;
+const slideShow = function () {
+    myInterval = setInterval(() => {
+        x -= 45;
+        updateGallery();
+    }, 5000);
+};
+
+slideShowEl.addEventListener("click", () => {
+    auto = !auto;
+    if (auto) slideShow();
+    else clearInterval(myInterval);
 });
